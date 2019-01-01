b9c3695d1d2618aae50c1102d413b49bf45b18d8
yaser omran <engineeryaseromran@gmail.com>
2019-01-01 19:18:29 +0200

    comitting changes

--- src/com/timbuchalka/StockList.java
+++ src/com/timbuchalka/StockList.java
@@ -20,7 +20,9 @@ public class StockList {
     public StockList() {
         this.list = new LinkedHashMap<>();
     }
-
+public void myMethod() {
+	
+}
     public int addStock(StockItem item) {
         if(item != null) {
             // check if already have quantities of this item
